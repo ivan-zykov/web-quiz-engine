9521031976cfafa027bc580e0d1238d1c197ed06
Ivan Zykov <ivan.v.zykov@gmail.com>
2026-07-15 11:29:24 +0200

    Define column names of User entity

--- src/main/kotlin/net/ivanvzykov/webquizengine/persistence/AppUserEntity.kt
+++ src/main/kotlin/net/ivanvzykov/webquizengine/persistence/AppUserEntity.kt
@@ -1,13 +1,20 @@
 package net.ivanvzykov.webquizengine.persistence
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 
 @Entity
 class AppUserEntity(
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
     var id: Int = 0,
+
+    @Column(name = "username")
     var username: String = "",
+
+    @Column(name = "password")
     var password: String = "",
 )
